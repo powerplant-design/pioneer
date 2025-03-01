7a0d8ff7811df035fe7882661cb5b2ee11a28a6b
[Powerplant] <hotline@powerplant.design>
2025-03-01 17:48:09 +1300

    Added thanks page

--- sw.js
+++ sw.js
@@ -2,11 +2,12 @@ const CACHE_NAME = "static-cache-v1";
 const FILES_TO_CACHE = [
     "/",
     "/index.html",
-    "/contact/index.html", // Add all pages you want to cache
-    "/base.css",
-    "/main.css",
-    "/normalize.css",
-    "/script.js",
+    "/contact/index.html",
+    "/thanks/index.html",
+    "/css/base.css",
+    "/css/main.css",
+    "/css/normalize.css",
+    "/js/script.js",
     "/images/pioneer-logo.png",
 ];
 
@@ -34,10 +35,18 @@ self.addEventListener("activate", (event) => {
             );
         })
     );
+    self.clients.claim();
 });
 
 // Fetch Requests & Serve from Cache
 self.addEventListener("fetch", (event) => {
+    const requestUrl = new URL(event.request.url);
+
+    // Bypass handling form submissions
+    if (requestUrl.pathname === "/contact" && event.request.method === "POST") {
+        return;
+    }
+
     event.respondWith(
         caches.match(event.request).then((response) => {
             return response || fetch(event.request);
